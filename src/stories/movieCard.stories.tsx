bd6b1124b3a011fa617795e8e458f8922e98b59a
Caoimhín <caoimhinarnott@hotmail.com>
2024-08-18 10:53:12 +0100

    Stories Commented out - Vercel deployment debug

--- src/stories/movieCard.stories.tsx
+++ src/stories/movieCard.stories.tsx
@@ -1,40 +1,40 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import MovieCard from "../components/movieCard";
-import SampleMovie from "./sampleData";
-import { MemoryRouter } from "react-router";
-import MoviesContextProvider from "../contexts/moviesContext";
-import { action } from "@storybook/addon-actions";
-import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
-import React from 'react';
+// import type { Meta, StoryObj } from '@storybook/react';
+// import MovieCard from "../components/movieCard";
+// import SampleMovie from "./sampleData";
+// import { MemoryRouter } from "react-router";
+// import MoviesContextProvider from "../contexts/moviesContext";
+// import { action } from "@storybook/addon-actions";
+// import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
+// import React from 'react';
 
-const meta = {
-  title: 'Home Page/MovieCard',
-  component: MovieCard,
-  decorators: [
-    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
-    (Story) => <MoviesContextProvider>{Story()}</MoviesContextProvider>,
-  ],
-} satisfies Meta<typeof MovieCard>;
+// const meta = {
+//   title: 'Home Page/MovieCard',
+//   component: MovieCard,
+//   decorators: [
+//     (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+//     (Story) => <MoviesContextProvider>{Story()}</MoviesContextProvider>,
+//   ],
+// } satisfies Meta<typeof MovieCard>;
 
-export default meta;
+// export default meta;
 
-type Story = StoryObj<typeof meta>;
+// type Story = StoryObj<typeof meta>;
 
-export const Basic: Story = {
-  args: {
-    action: (movie ) => <AddToFavouritesIcon {...movie} />,
-    movie: SampleMovie,
+// export const Basic: Story = {
+//   args: {
+//     action: (movie ) => <AddToFavouritesIcon {...movie} />,
+//     movie: SampleMovie,
 
-  }
+//   }
 
-};
-Basic.storyName = "Default";
+// };
+// Basic.storyName = "Default";
 
-const sampleNoPoster = { ...SampleMovie, poster_path: undefined };
-export const Exceptional: Story = {
-  args: {
-    movie: sampleNoPoster,
-    action: (movie ) => <AddToFavouritesIcon {...movie} />,
-  }
-};
-Exceptional.storyName = "Exception";
+// const sampleNoPoster = { ...SampleMovie, poster_path: undefined };
+// export const Exceptional: Story = {
+//   args: {
+//     movie: sampleNoPoster,
+//     action: (movie ) => <AddToFavouritesIcon {...movie} />,
+//   }
+// };
+// Exceptional.storyName = "Exception";
